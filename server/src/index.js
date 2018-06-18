6c1a371f75b4d1d2d9f8df1e92c226913dc53576
gorkamolero <gorka.molero@gmail.com>
2018-06-18 19:50:54 +0200

    New submit, password and routes

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
 import { uploadUsers } from './dbTmp';
 
 mongoose.connect('mongodb://localhost/blitz')
@@ -23,6 +24,7 @@ const userSchema = new mongoose.Schema({
   lastName: String,
   job: String,
   age: Number,
+  password: String,
   posts: [postSchema]
 })
 
@@ -35,11 +37,12 @@ const User = mongoose.model('User', userSchema)
 
 /* Starting app */
 const app = express();
-
+app.use(bodyParser.json());
 
 const findPosts = async (req, res) => {
   let posts = await Post.find()
-    .populate({ path: 'author', model: User })
+                        .populate({ path: 'author', model: User })
+                        .sort('date')
 
   // Send response
   res.json(posts)
@@ -68,19 +71,61 @@ const findUsers = async (req, res) => {
 
 const findPostByUserSlug = async (req, res) => {
   const { slug } = req.params
+  console.log(slug)
   const author = await User.findOne({ slug })
 
-  author.posts = await Post.find({ userId: author.id })
+  author.posts = await Post.find({ author: author.id }).sort('date')
 
   res.json(author)
+}
+
+const findLastDate = async (req, res) => {
+  //const { date } = req.params
+  const post = await Post.findOne().sort({ field: 'asc', _id: -1 }).limit(1)
+  res.json(post.date)
+}
+
+const getPass = async (req, res) => {
+  const { slug } = req.params
+  const user = await User.findOne({ slug })
+  console.log(user.password)
+  res.send(user.password)
+}
+
+const submitPost = async (req, res) => {
+  const { authorSlug } = req.params
+  const author = await User.findOne({ authorSlug })
+  const authorId = author._id
+  console.log(authorId)
+
+  const post = {
+    'title': req.body.title,
+    'date': req.body.date,
+    'slug': req.body.slug,
+    'author': authorId,
+    'text': req.body.text
+  }
+
+  const newPost = new Post(post)
+
+  newPost.save(err => {
+    if (err) return handleError(err);
+    console.log('Saved post: ' + newPost.title)
+
+    res.json(newPost)
+  })
 }
 
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/api/users/', findUsers)
 app.get('/api/users/:slug', findPostByUserSlug)
+app.get('/api/login/:slug', getPass)
 app.get('/api/posts/', findPosts)
 app.get('/api/posts/:slug', findPost)
+app.get('/api/lastPost/', findLastDate)
+
+app.post('/api/posts/', submitPost)
 
 
 app.listen(8080, () => console.log('Yo! I\'m Blitz!\'s first blog! '));
